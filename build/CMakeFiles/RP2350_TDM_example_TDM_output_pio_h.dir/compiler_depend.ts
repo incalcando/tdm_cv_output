25c84b315465a20e0fcb6365541efa5d57c6fe2f
incalcando <s.lexer@incalcando.com>
2025-02-07 21:22:49 +0000

    Updated .gitignore to exclude build

--- build/CMakeFiles/RP2350_TDM_example_TDM_output_pio_h.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for RP2350_TDM_example_TDM_output_pio_h.
--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/sebastian/Instruo/tdm_output_example/build/_deps/picotool/"
-};
